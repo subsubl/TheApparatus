474701decadfea785bfc9c1c45f23ee23c7e2d5a
subsubl <subsubl@gmail.com>
2026-09-21 20:47:40 +0200

    fix(pi): remove --video-margin-ratio=0 which is unsupported in older mpv versions

--- pi/player_a.py
+++ pi/player_a.py
@@ -35,7 +35,6 @@ def main() -> int:
             "--video-sync=display-resample",
             "--video-aspect-override=4:3", # PAL SD 4:3 mixer aspect ratio
             "--override-display-fps=50",   # 50 Hz PAL frame rate sync
-            "--video-margin-ratio=0",
             *vo_args,
             "--input-default-bindings=no",
             "--input-vo-keyboard=no",
